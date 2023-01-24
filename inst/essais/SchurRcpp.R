f01164eb106c10cf4762f29514eaa7f682a4e7c9
stla <laurent_step@yahoo.fr>
2023-01-24 19:56:35 +0100

    essai benchmark

--- inst/essais/SchurRcpp.R
+++ inst/essais/SchurRcpp.R
@@ -1,3 +1,26 @@
-x <- jack:::SchurPolRcpp(n = 3L, lambda = c(4L, 0L, 0L))
+
+x <- jack:::SchurPolRcpp(n = 3L, lambda = 4L)
 qspray::qsprayMaker(x[["exponents"]], x[["coeffs"]])
 jack::SchurPol(3L, 4L)
+
+
+library(microbenchmark)
+library(jack)
+library(qspray)
+
+rcpp <- function(n, lambda) {
+  x <- jack:::SchurPolRcpp(as.integer(n), as.integer(lambda))
+  qsprayMaker(x[["exponents"]], x[["coeffs"]])
+}
+
+julia <- Jack_julia()
+
+n <- 5
+lambda <- c(4, 3, 3)
+microbenchmark(
+  Rcpp  = rcpp(n, lambda),
+  Julia = julia$SchurPol(n, lambda),
+  times = 6L
+)
+
+JuliaConnectoR::stopJulia()
