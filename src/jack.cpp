4f71c8af3ed5b6d03616d225cbc9ea5b8ad4904c
stla <laurent_step@yahoo.fr>
2023-01-25 13:54:12 +0100

    started jac

--- src/jack.cpp
+++ src/jack.cpp
@@ -35,6 +35,7 @@ public:
   }
 };
 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 int _N(Partition lambda, Partition mu) {
@@ -88,6 +89,8 @@ Poly<CoeffT> polyAdd(Poly<CoeffT> P1, Poly<CoeffT> P2) {
 }
 
 template Zpoly polyAdd<int>(Zpoly, Zpoly);
+template Qpoly polyAdd<gmpq>(Qpoly, Qpoly);
+
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -152,6 +155,7 @@ Poly<CoeffT> polyMult(const Poly<CoeffT> P1, const Poly<CoeffT> P2) {
 }
 
 template Zpoly polyMult<int>(const Zpoly, const Zpoly);
+template Qpoly polyMult<gmpq>(const Qpoly, const Qpoly);
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -163,6 +167,7 @@ Poly<CoeffT> zeroPoly() {
 }
 
 template Zpoly zeroPoly<int>();
+template Qpoly zeroPoly<gmpq>();
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -177,6 +182,20 @@ Poly<CoeffT> unitPoly() {
 }
 
 template Zpoly unitPoly<int>();
+template Qpoly unitPoly<gmpq>();
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+template <typename CoeffT>
+Poly<CoeffT> constantPoly(CoeffT a) {
+  Poly<CoeffT> out;
+  Powers pows(0);
+  out[pows] = a;
+  return out;
+}
+
+template Qpoly constantPoly<gmpq>(gmpq);
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -192,6 +211,7 @@ Poly<CoeffT> lonePoly(const int n) {
 }
 
 template Zpoly lonePoly<int>(const int);
+template Qpoly lonePoly<gmpq>(const int);
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -215,6 +235,7 @@ Poly<CoeffT> polyPow(const Poly<CoeffT> P, int n) {
 }
 
 template Zpoly polyPow<int>(const Zpoly, int);
+template Qpoly polyPow<gmpq>(const Qpoly, int);
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -342,6 +363,90 @@ gmpq _betaratio(Partition kappa, Partition mu, int k, gmpq alpha) {
   }
   return alpha * prod1 * prod2 * prod3;
 }
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+int weight(Partition mu) {
+  int w = 0;
+  int musize = mu.size();
+  for(int i = 0; i < musize; i++) {
+    w += mu[i];
+  }
+  return w;
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+typedef std::unordered_map<std::pair<int, int>, Qpoly, pairHasher> Qij;
+
+Qpoly jac(
+  Partition lambda, Qij S, gmpq alpha,
+  int m, int k, Partition mu, Partition nu, gmpq beta
+) {
+  const int nusize = nu.size();
+  if(nusize == 0 || nu[0] == 0 || m == 0) {
+    return unitPoly<gmpq>();
+  }
+  if(nusize > m && nu[m] > 0) {
+    return zeroPoly<gmpq>();
+  }
+  gmpq oneq(1, 1);
+  if(m == 1){
+    gmpq al = 0;
+    gmpq prod(1, 1);
+    for(int i = 1; i < nu[1]; i++) {
+      al += alpha;
+      prod *= (al + oneq);
+    }
+    return polyMult<gmpq>(
+      constantPoly<gmpq>(prod), polyPow<gmpq>(lonePoly<gmpq>(1), nu[0])
+    );
+  }
+  int N = _N(lambda, nu);
+  std::pair<int, int> Nm = std::make_pair(N, m);
+  if(S.contains(Nm)) {
+    return S[Nm];
+  }
+  Qpoly s = polyMult<gmpq>(
+    jac(lambda, S, alpha, m-1, 0, nu, nu, oneq),
+    polyMult<gmpq>(
+      constantPoly<gmpq>(beta),
+      polyPow<gmpq>(lonePoly<gmpq>(m), weight(mu) - weight(nu))
+    )
+  );
+  int i = k;
+  while(nusize >= i && nu[i-1] > 0) {
+    if(nusize == i || nu[i-1] > nu[i]) {
+      Partition _nu(nu);
+      _nu[i-1] = nu[i-1] - 1;
+      if(nu[i-1] > 1) {
+        s = polyAdd<int>(
+          s, polyMult<int>(lonePoly<int>(m), sch(lambda, S, m, i, _nu))
+        );
+      } else {
+        s = polyAdd<int>(
+          s, polyMult<int>(lonePoly<int>(m), sch(lambda, S, m-1, 1, _nu))
+        );
+      }
+    }
+    i++;
+  }
+  if(k == 1) {
+    S[Nm] = s;
+  }
+  return s;
+}
+
+Qpoly JackPol(int n, Partition lambda) {
+  Qij S;
+  return sch(lambda, S, n, 1, lambda);
+}
+
+
+
+
 
 // [[Rcpp::export]]
 void test() {
