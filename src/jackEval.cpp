604f130788dcc0ef043e95d0c8976e424239478d
stla <laurent_step@yahoo.fr>
2023-05-08 18:20:28 +0200

    SchurEval

--- src/jackEval.cpp
+++ src/jackEval.cpp
@@ -78,10 +78,55 @@ std::string JackEvalRcpp(
 }
 
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+gmpq schEval(
+  std::vector<gmpq> x, Partition lambda, Gij S, int m, int k, Partition nu
+) {
+  const int nusize = nu.size();
+  if(nusize == 0 || nu[0] == 0 || m == 0) {
+    return gmpq(1);
+  }
+  if(nusize > m && nu[m] > 0) {
+    return gmpq(0);
+  }
+  if(m == 1){
+    return gmpqpow(x[0], nu[0]);
+  }
+  int N = _N(lambda, nu);
+  std::pair<int, int> Nm = std::make_pair(N, m);
+  if(auto search = S.find(Nm); search != S.end()) {
+    return S[Nm];
+  }
+  gmpq s = schEval(x, lambda, S, m-1, 1, nu);
+  int i = k;
+  while(nusize >= i && nu[i-1] > 0) {
+    if(nusize == i || nu[i-1] > nu[i]) {
+      Partition _nu(nu);
+      _nu[i-1] = nu[i-1] - 1;
+      if(nu[i-1] > 1) {
+        s = s + x[m-1] * schEval(x, lambda, S, m, i, _nu);
+      } else {
+        s = s + x[m-1] * schEval(x, lambda, S, m-1, 1, _nu);
+      }
+    }
+    i++;
+  }
+  if(k == 1) {
+    S[Nm] = s;
+  }
+  return s;
+}
+
+gmpq SchurEval(std::vector<gmpq> x, Partition lambda) {
+  Gij S;
+  return schEval(x, lambda, S, x.size(), 1, lambda);
+}
+
 // [[Rcpp::export]]
 void test() {
   std::vector<gmpq> x = {gmpq(2), gmpq(3), gmpq(4), gmpq(5)};
   std::vector<int> lambda = {3, 1};
   gmpq alpha(5, 2);
-  Rcpp::Rcout << JackEval(x, lambda, alpha);
+  Rcpp::Rcout << SchurEval(x, lambda);
 }
