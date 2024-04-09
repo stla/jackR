3ebd3fb24698a395adcd07cddf3b7eca7b60ddc0
stla <laurent_step@yahoo.fr>
2024-04-09 07:28:02 +0200

    unit test polynomial coefficients

--- tests/testthat/test-jackSymbolic.R
+++ tests/testthat/test-jackSymbolic.R
@@ -31,7 +31,15 @@ test_that("JackSymPol Q", {
 test_that("JackSymPol is symmetric", {
   n <- 3
   lambda <- c(4, 2, 2)
-  alpha <- gmp::as.bigq("2/3")
   symbolicJackPolynomial <- JackSymPol(n, lambda)
   expect_silent(MSPcombination(symbolicJackPolynomial, check = TRUE))
+})
+
+test_that("JackSymPol has polynomial coefficients only", {
+  n <- 5
+  lambda <- c(4, 4, 3, 2, 1)
+  J <- JackSymPol(n, lambda)
+  expect_true(
+   all(vapply(J@coeffs, isPolynomial, logical(1L)))
+  )
 })
