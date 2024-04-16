64bde8e54808a908e2fcff4dd82168009e3243e0
stla <laurent_step@yahoo.fr>
2024-04-16 20:49:49 +0200

    modified Valgrind GA

--- inst/benchmarks/benchmarks.R
+++ inst/benchmarks/benchmarks.R
@@ -26,3 +26,12 @@ microbenchmark(
 # benchmarking Jack/jackSymbolicPol evaluated at alpha
 # mean                 621.9 ms
 # std dev              50.79 ms
+
+# Julia is faster:
+#
+# julia> @benchmark JackPolynomial(5, [4; 2; 2; 1])
+# BenchmarkTools.Trial: 15 samples with 1 evaluation.
+#  Range (min … max):  261.262 ms … 498.055 ms  ┊ GC (min … max): 21.28% … 27.22%
+#  Time  (median):     342.574 ms               ┊ GC (median):    18.62%
+#  Time  (mean ± σ):   349.626 ms ±  65.011 ms  ┊ GC (mean ± σ):  22.29% ±  5.76%
+
