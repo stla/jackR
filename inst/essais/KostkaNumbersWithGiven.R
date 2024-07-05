9d27e87859d9b6e345148d9122131e249de4c1d8
stla <laurent_step@yahoo.fr>
2024-07-05 06:03:09 +0200

    KostkaNumbersWithGivenLambda

--- inst/essais/KostkaNumbersWithGiven.R
+++ inst/essais/KostkaNumbersWithGiven.R
@@ -1,71 +1,62 @@
-KostkaNumbersWithGivenLambda <- function(lambda) {
+KostkaNumbersWithGivenLambda <- function(lambda, output = "vector") {
+  stopifnot(isPartition(lambda))
+  output <- match.arg(output, c("vector", "list"))
   lambda <- removeTrailingZeros(as.integer(lambda))
   mus <- rev(listOfDominatedPartitions(lambda))
-  kNumbers <- seq_along(mus)
+  kNumbers <- rep(NA_integer_, length(mus))
   musAsStrings <-
     vapply(mus, partitionAsString, character(1L), USE.NAMES = FALSE)
   names(mus) <- names(kNumbers) <- musAsStrings
   ellLambda <- length(lambda)
-  if(ellLambda == 1L) {
+  if(ellLambda == 0L || ellLambda == 1L) {
     kNumbers <- 1L
-    return(kNumbers)
-  }
-  # psums <- cumsum(tail(lambda, -2L))
-  # ellPsums <- ellLambda - 2L
-  # seq_Psums <- seq_len(ellPsums)
-  # .isDominated <- function(mu){
-  #   for(i in seq_Psums){
-  #     if(sum(mu[1L:i]) > psums[i]){
-  #       return(FALSE)
-  #     }
-  #   }
-  #   TRUE
-  # }
-  lambdap <- conjugate(lambda)
-  nlambda <- sum(seq_len(ellLambda - 1L) * tail(lambda, -1L))
-  nlambdap <- sum(seq_len(lambda[1L] - 1L) * tail(lambdap, -1L))
-  elambda <- as.bigq(nlambdap - nlambda)
-  kNumbers[1L] <- 1L
-  for(muAsString in tail(musAsStrings, -1L)) {
-    mu <- mus[[muAsString]]
-    mup <- conjugate(mu)
-    ellMu <- length(mu)
-    nmu <- sum(seq_len(ellMu - 1L) * tail(mu, -1L))
-    nmup <- sum(seq_len(mu[1L] - 1L) * tail(mup, -1L))
-    emu <- as.bigq(nmup - nmu)
-    ee <- elambda - emu
-    x <- as.bigq(0L)
-    for(i in 1L:(ellMu-1L)) {
-      for(j in (i+1L):ellMu) {
-        dmuij <- mu[i] - mu[j]
-        for(t in seq_len(mu[j])) {
+  } else {
+    lambdap <- conjugate(lambda)
+    nlambda <- sum(seq_len(ellLambda - 1L) * tail(lambda, -1L))
+    nlambdap <- sum(seq_len(lambda[1L] - 1L) * tail(lambdap, -1L))
+    elambda <- nlambdap - nlambda
+    kNumbers[1L] <- 1L
+    for(muAsString in tail(musAsStrings, -1L)) {
+      mu <- mus[[muAsString]]
+      mup <- conjugate(mu)
+      ellMu <- mup[1L]
+      i_ <- seq_len(ellMu - 1L)
+      nmu <- sum(i_ * tail(mu, -1L))
+      nmup <- sum(seq_len(mu[1L] - 1L) * tail(mup, -1L))
+      emu <- nmup - nmu
+      ee <- as.bigq(elambda - emu)
+      x <- 0L
+      for(i in i_) {
+        mu_i <- mu[i]
+        for(j in (i+1L):ellMu) {
+          mu_j <- mu[j]
+          dmuij <- mu_i - mu_j
           kappa <- mu
-          kappa[i] <- mu[i] + t
-          kappa[j] <- mu[j] - t
-          muOrd <- removeTrailingZeros(sort(kappa, decreasing = TRUE))
-          if(partitionAsString(muOrd) %in% musAsStrings){
-            x <- x + (dmuij + 2*t) /
-              ee *
-              kNumbers[partitionAsString(muOrd)]
+          for(t in seq_len(mu_j)) {
+            kappa[i] <- mu_i + t
+            kappa[j] <- mu_j - t
+            kappaOrd <- removeTrailingZeros(sort(kappa, decreasing = TRUE))
+            kappaOrdAsString <- partitionAsString(kappaOrd)
+            if(kappaOrdAsString %in% musAsStrings){
+              x <- x + kNumbers[kappaOrdAsString] * (dmuij + 2*t) / ee
+            }
           }
         }
       }
+      kNumbers[muAsString] <- as.integer(x)
     }
-    kNumbers[muAsString] <- as.integer(x)
+  }
+  if(output == "list") {
+    kNumbers <- mapply(
+      function(kNumber, mu) {
+        list("mu" = mu, "value" = kNumber)
+      },
+      kNumbers, mus,
+      USE.NAMES = TRUE, SIMPLIFY = FALSE
+    )
   }
   kNumbers
 }
 
+KostkaNumbersWithGivenLambda(c(2,2,1))
 
-.isDominated <- function(mu, lambda){
-  # assumptions:
-  # sum(mu) == sum(lambda)
-  # length(mu) == length(lambda) == sum(mu)
-  ellMu <- match(0L, mu, nomatch = length(mu) + 1L) - 1L
-  for(i in seq_len(ellMu)){
-    if(sum(mu[1L:i]) > sum(lambda[1L:i])){
-      return(FALSE)
-    }
-  }
-  TRUE
-}
