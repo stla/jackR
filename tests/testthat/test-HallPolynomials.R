356cfa2a2e9981184885b431a2d9e8c6e044181e
stla <laurent_step@yahoo.fr>
2024-07-13 19:14:30 +0200

    unit test HallPolynomials

--- tests/testthat/test-HallPolynomials.R
+++ tests/testthat/test-HallPolynomials.R
@@ -7,3 +7,17 @@ test_that("A Hall polynomial (comparison with Sage)", {
   expect_true(hallPoly == expected)
 })
 
+test_that("Leading coefficient of Hall polynomial is Littlewood-Richardson", {
+  mu <- c(3, 1)
+  nu <- c(2, 1)
+  lrCoeffs <- LRmult(mu, nu, output = "dataframe")
+  coeffs <- lrCoeffs[, "coeff"]
+  lambdasAsStrings <- lrCoeffs[, "lambda"]
+  names(coeffs) <- lambdasAsStrings
+  hallPolys <- HallPolynomials(mu, nu)
+  lambdasAsStrings <- names(hallPolys)
+  leadingCoeffs <- gmp::c_bigq(lapply(hallPolys, function(x) {
+    leadingCoefficient(x[["polynomial"]])
+  }))
+  expect_true(all(coeffs[lambdasAsStrings] == leadingCoeffs))
+})
