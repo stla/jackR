0f47586163c0ce16b7b9485f1ee9cabbf4e4a68e
stla <laurent_step@yahoo.fr>
2024-08-13 11:39:50 +0200

    unit test HLcombinationP symbolic Jack

--- tests/testthat/test-HLcombinations.R
+++ tests/testthat/test-HLcombinations.R
@@ -1,4 +1,4 @@
-test_that("HLcombinationP", {
+test_that("HLcombinationP - Jack polynomial", {
   lambda <- c(2, 2, 1)
   alpha <- "2/5"
   which <- "C"
@@ -13,4 +13,19 @@ test_that("HLcombinationP", {
   obtained <- Reduce(`+`, toAdd)
   Jpoly2 <- as(Jpoly, "symbolicQspray")
   expect_true(obtained == Jpoly2)
+})
+
+test_that("HLcombinationP - Jack symbolic polynomial", {
+  lambda <- c(2, 2, 1)
+  which <- "Q"
+  n <- sum(lambda)
+  JsymPoly <- JackSymPol(n, lambda, which)
+  combo <- HLcombinationP(JsymPoly)
+  toAdd <- lapply(combo, function(lst) {
+    mu <- lst[["lambda"]]
+    coeff <- lst[["coeff"]]
+    coeff * HallLittlewoodPol(n, mu, "P")
+  })
+  obtained <- Reduce(`+`, toAdd)
+  expect_true(obtained == JsymPoly)
 })
