bec19efee6d08d81b9eb2b16e3f86cac19e47b84
stla <laurent_step@yahoo.fr>
2024-08-20 01:07:06 +0200

    unit test psPolynomial

--- tests/testthat/helper-00.R
+++ tests/testthat/helper-00.R
@@ -3,3 +3,7 @@ mvpEqual <- function(pol1, pol2){
     identical(pol1[["power"]], pol2[["power"]]) &&
     isTRUE(all.equal(pol1[["coeffs"]], pol2[["coeffs"]]))
 }
+
+Print <- function(x) {
+  capture.output(x)
+}
